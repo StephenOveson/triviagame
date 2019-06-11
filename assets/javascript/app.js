f0114f2628ac72b47c45c844a9c15517e446db80
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-10 20:14:40 -0600

    for loop created calling api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,24 @@ let triviaA = $('#triviaA');
 let triviaTimer = $('#triviaTimer');
 let triviaUpdate = $('#triviaUpdate');
 let triviaTitle = $('#triviaTitle');
+let queryURL = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy";
 
 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    let results = response.results;
+    for (let x = 0; x < results.length; x++) {
+        let result = results[x];
+        triviaQ.html(result.question);
+        let randomAnswer = Math.floor(Math.random() * result.incorrect_answers.length + 1);
+        result.incorrect_answers.splice(randomAnswer, 0, result.correct_answer);
+        triviaA.html(result.incorrect_answers);
+        console.log(result.incorrect_answers);
+        debugger;
+    }
+});
 
 
 
