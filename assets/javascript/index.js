a66fabf1e19cc99c26c15b567c846765d94939e5
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-13 00:13:45 -0600

    adjustment to url for website

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,5 +2,5 @@ $('#submit').on('click', function(){
     let amount = $("#trivia_amount").val()
     let category = $('#category').val()
     let difficulty = $('#difficulty').val()
-    window.location.href = "/game.html?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty;
+    window.location.href = "/triviagame/game.html?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty;
 })
